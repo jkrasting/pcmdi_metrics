1a82193d5033e6fddc743996c50a7d4b047c9ebb
Charles Doutriaux <doutriaux1@llnl.gov>
2015-03-20 13:04:43 -0700

    updated test results

--- test/test_suite.py
+++ test/test_suite.py
@@ -5,17 +5,17 @@ sys.path.append(os.path.join(pth,"graphics"))
 
 suite= unittest.TestSuite()
 suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","basic_test_parameters_file.py")))
-#suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","units_test.py")))
-#suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","nosftlf_test.py")))
-#suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","gensftlf_test.py")))
-#suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","keep_going_on_error_varname_test.py")))
-#try:
-#    # If we have vcs we can test graphics
-#    import vcs,test_portrait
-#    suite.addTest(test_portrait.TestGraphics("test_portrait"))
-#except Exception,err:
-#    print "ERROR import vcs, skipping graphics test..."
-#    pass
-#
+suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","units_test.py")))
+suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","nosftlf_test.py")))
+suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","gensftlf_test.py")))
+suite.addTest(test_from_param.TestFromParam(os.path.join(pth,"pcmdi","keep_going_on_error_varname_test.py")))
+try:
+    # If we have vcs we can test graphics
+    import vcs,test_portrait
+    suite.addTest(test_portrait.TestGraphics("test_portrait"))
+except Exception,err:
+    print "ERROR import vcs, skipping graphics test..."
+    pass
+
 unittest.TextTestRunner(verbosity=2).run(suite)
 
